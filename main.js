996f4d56dc4f1c4e461ddd7e43a768b680ff3746
mad rasa 7 <hvp.mrb@gmail.com>
2024-08-11 23:20:13 -0700

    fixs

--- main.js
+++ main.js
@@ -4,60 +4,60 @@ const path = require('path');
 require('dotenv').config();
 
 function createWindow() {
-  const win = new BrowserWindow({
-    width: 1024,
-    height: 768,
-    minWidth: 650,
-    minHeight: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-      nodeIntegration: true,
-      contextIsolation: false
-    }
-  });
+	const win = new BrowserWindow({
+		width: 1024,
+		height: 768,
+		minWidth: 650,
+		minHeight: 600,
+		webPreferences: {
+			preload: path.join(__dirname, 'preload.js'),
+			nodeIntegration: true,
+			contextIsolation: false
+		}
+	});
 
-  win.loadFile('index.html');
+	win.loadFile('index.html');
 }
 
 app.whenReady().then(() => {
 
-  createWindow();
+	createWindow();
 
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
-    }
-  });
+	app.on('activate', () => {
+		if (BrowserWindow.getAllWindows().length === 0) {
+			createWindow();
+		}
+	});
 
-  Menu.setApplicationMenu(null);
+	Menu.setApplicationMenu(null);
 
-  // Context Menu
-  ipcMain.on('show-context-menu', (event) => {
-    const template = [
-      {
-        label: 'Scan Files',
-        click: () => { event.sender.send('context-menu-command', 'scan-file'); }
-      },
-      {
-        label: 'Scan Folder',
-        click: () => { event.sender.send('context-menu-command', 'scan-folder'); }
-      },
-      {
-        label: "Dev Tools",
-        role: 'toggleDevTools'
-      }
-    ];
-    const menu = Menu.buildFromTemplate(template);
-    menu.popup(BrowserWindow.fromWebContents(event.sender));
-  });
-});
-
-ipcMain.handle('get-api-key', () => {
-  return process.env.VIRUSTOTAL_API_KEY;
+	// Context Menu
+	ipcMain.on('show-context-menu', (event) => {
+		const template = [
+			{
+				label: 'Scan Files',
+				click: () => { event.sender.send('context-menu-command', 'scan-file'); }
+			},
+			{
+				label: 'Scan Folder',
+				click: () => { event.sender.send('context-menu-command', 'scan-folder'); }
+			},
+			{
+				label: "Dev Tools",
+				role: 'toggleDevTools'
+			}
+		];
+		const menu = Menu.buildFromTemplate(template);
+		menu.popup(BrowserWindow.fromWebContents(event.sender));
+	});
 });
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
+});
+
+ipcMain.handle('get-api-key', () => {
+	return process.env.VIRUSTOTAL_API_KEY;
 });
