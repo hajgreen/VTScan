e16ac1866e1b91ababd5390a17c13d4d951546ff
mad rasa 7 <hvp.mrb@gmail.com>
2024-08-11 21:33:36 -0700

    main and first commit

---
+++ main.js
@@ -0,0 +1,58 @@
+const { app, BrowserWindow, Menu, ipcMain } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1024,
+    height: 768,
+    minWidth: 650,
+    minHeight: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(() => {
+
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+
+  Menu.setApplicationMenu(null);
+
+  // Context Menu
+  ipcMain.on('show-context-menu', (event) => {
+    const template = [
+      {
+        label: 'Scan File',
+        click: () => { event.sender.send('context-menu-command', 'scan-file'); }
+      },
+      {
+        label: 'Scan Folder',
+        click: () => { event.sender.send('context-menu-command', 'scan-folder'); }
+      },
+      {
+        label: "Dev Tools",
+        role: 'toggleDevTools'
+      }
+    ];
+    const menu = Menu.buildFromTemplate(template);
+    menu.popup(BrowserWindow.fromWebContents(event.sender));
+  });
+});
+
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
---
+++ renderer.js
@@ -0,0 +1,241 @@
+const dropArea = document.getElementById('drop-area');
+const resultsContainer = document.getElementById('results');
+const fileInput = document.getElementById('file-input');
+const folderInput = document.getElementById('folder-input');
+const fileSelectButton = document.getElementById('file-select-button');
+const folderSelectButton = document.getElementById('folder-select-button');
+const crypto = require('crypto');
+const path = require('path');
+const { ipcRenderer } = require('electron');
+
+
+//MenuBar and Context Menu
+window.addEventListener('contextmenu', (e) => {
+	e.preventDefault();
+	ipcRenderer.send('show-context-menu');
+});
+
+ipcRenderer.on('context-menu-command', (event, command) => {
+	switch (command) {
+		case 'scan-file':
+			document.getElementById('file-input').click();
+			break;
+		case 'scan-folder':
+			document.getElementById('folder-input').click();
+			break;
+	}
+});
+
+
+// Event listener برای درگ و دراپ فایل
+dropArea.addEventListener('dragover', (event) => {
+	event.preventDefault();
+	dropArea.style.background = '#f0f0f0';
+});
+
+dropArea.addEventListener('dragleave', () => {
+	dropArea.style.background = '';
+});
+
+dropArea.addEventListener('drop', async (event) => {
+	resultsContainer.innerHTML = "";
+
+	event.preventDefault();
+	dropArea.style.background = '';
+
+	const files = event.dataTransfer.files;
+	resultsContainer.innerHTML += `<h4 style="margin-bottom: 20px;"><strong>Files count: ${files.length} </strong></h4>`;
+
+	if (files.length > 0) {
+
+		console.log(files);
+
+		if (files[0].type != "application/octet-stream" && files[0].type != "") {
+			await handleMultipleFiles(files);
+		}
+		else {
+			alert("This is file or folder not support (please add files or folder with button)!");
+		}
+	}
+});
+
+// Event listener برای انتخاب فایل از طریق دکمه
+fileSelectButton.addEventListener('click', () => {
+	resultsContainer.innerHTML = "";
+	fileInput.click();
+});
+
+fileInput.addEventListener('change', async (event) => {
+	const files = event.target.files;
+	if (files.length > 0) {
+		await handleMultipleFiles(files);
+	}
+});
+
+// Event listener برای انتخاب پوشه از طریق دکمه
+folderSelectButton.addEventListener('click', () => {
+	resultsContainer.innerHTML = "";
+	folderInput.click();
+});
+
+folderInput.addEventListener('change', async (event) => {
+	const files = event.target.files;
+
+	resultsContainer.innerHTML = "";
+	resultsContainer.innerHTML += `<h4 style="margin-bottom: 20px;"><strong>Files count: ${files.length} </strong></h4>`;
+
+	const executableExtensions = ['.exe', '.bat', '.cmd', '.com', '.msi', '.scr', '.vbs', '.js', '.jse', '.wsf', '.wsh', '.ps1', '.gadget', '.msc', '.pif', '.reg', '.inf', '.jar', '.py'];
+
+	for (let file of files) {
+		const ext = path.extname(file.name).toLowerCase();
+		if (executableExtensions.includes(ext)) {
+			await handleFile(file);
+		}
+	}
+});
+
+// Function برای پردازش فایل‌های انتخابی
+async function handleMultipleFiles(files) {
+	ShowLoading(true);
+	resultsContainer.innerHTML = "";
+	resultsContainer.innerHTML += `<h4 style="margin-bottom: 20px;"><strong>Files count: ${files.length} </strong></h4>`;
+
+	for (let file of files) {
+		await handleFile(file);
+	}
+
+	ShowLoading(false);
+}
+
+// Function برای پردازش فایل انتخابی
+async function handleFile(file) {
+
+	// نمایش نام فایل
+	const fileName = file.name;
+	const fileSection = document.createElement('div');
+	fileSection.className = 'file-section';
+	resultsContainer.appendChild(fileSection);
+
+	// Read file as buffer
+	const buffer = await file.arrayBuffer();
+	const fileData = new Uint8Array(buffer);
+
+	// محاسبه هش فایل
+	const hash = calculateHash(fileData);
+
+	// نمایش هش فایل
+	fileSection.innerHTML += `<p><strong>File Hash (SHA-1):</strong> ${hash}</p>`;
+
+	// بررسی هش در VirusTotal
+	await checkFileHash(hash, fileName, fileSection);
+}
+
+function calculateHash(fileData) {
+	return crypto.createHash('sha1').update(fileData).digest('hex');
+}
+
+function ShowLoading(bool) {
+
+	if (bool) {
+		document.body.style.overflowY = "hidden";
+		document.getElementById('loading').style.display = 'flex';
+	}
+	else {
+		document.body.style.overflowY = "auto";
+		document.getElementById('loading').style.display = 'none';
+	}
+}
+
+async function checkFileHash(hash, fileName, fileSection) {
+
+	ShowLoading(true);
+
+	const apiKey = '5091c939d83c3709cb9097665055143abd20b61d31cc361a94d92d57719a74d1';
+	const url = "https://www.virustotal.com/api/v3/files/" + hash;
+
+	try {
+
+		const response = await fetch(url, {
+			method: 'GET',
+			headers: {
+				'x-apikey': apiKey
+			}
+		});
+
+		if (response.ok) {
+			const result = await response.json();
+			displayResults(result.data.attributes, fileName, fileSection);
+		}
+		else if (response.status === 404) {
+			fileSection.innerHTML += `<p>File with hash ${hash} not found in VirusTotal database.</p>`;
+		}
+		else {
+			const errorDetails = await response.json();
+			fileSection.innerHTML += `<p>Error checking file hash: ${errorDetails.error.message}</p>`;
+		}
+	} catch (error) {
+		fileSection.innerHTML += `<p>Network or fetch error: ${error.message}</p>`;
+	}
+}
+
+function displayResults(attributes, fileName, fileSection) {
+
+	const { last_analysis_stats, last_analysis_results } = attributes;
+	const totalAVs = last_analysis_stats.harmless + last_analysis_stats.malicious + last_analysis_stats.suspicious + last_analysis_stats.undetected + last_analysis_stats.timeout;
+	const maliciousAVs = last_analysis_stats.malicious;
+
+	fileSection.innerHTML += `<p><strong>File Name: </strong> ${fileName}</p>`;
+
+	// نمایش اطلاعات کلی
+	fileSection.innerHTML += `
+        <p><strong>Scan result: </strong><span style="color:${maliciousAVs > 1 ? "red" : "#4caf50"}; font-size: 18px;">${maliciousAVs} of ${totalAVs}</span></p>
+    `;
+
+	// ایجاد آکاردیون برای نتایج دقیق آنتی ویروس‌ها
+	const accordionId = `accordion-${fileName.replace(/\s+/g, '-')}`;
+
+	const accordion = document.createElement('div');
+	accordion.className = 'accordion';
+	accordion.id = accordionId;
+
+	let accordionContent = `
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="heading-${accordionId}">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-${accordionId}" aria-expanded="true" aria-controls="collapse-${accordionId}">
+                    Click for Details
+                </button>
+            </h2>
+            <div id="collapse-${accordionId}" class="accordion-collapse collapse" aria-labelledby="heading-${accordionId}" data-bs-parent="#${accordionId}">
+                <div class="accordion-body row">
+    `;
+
+	// اضافه کردن نتایج دقیق به آکاردیون
+	Object.keys(last_analysis_results).forEach((engine, index) => {
+		const result = last_analysis_results[engine];
+		const statusClass = result.category === 'undetected' ? 'status-clean' :
+			result.category === 'malicious' ? 'status-malicious' : 'status-unknown';
+
+		accordionContent += `
+            <div class="col-lg-4 main-card">
+                <div class="card">
+					<div class="d-flex justify-content-between align-items-center">
+						<span style="font-weight: 600;">${engine}</span>
+						<span class="status ${statusClass}">${result.result || result.category}</span>
+					</div>
+                </div>
+            </div>
+        `;
+	});
+
+	// پایان آکاردیون
+	accordionContent += `
+                </div>
+            </div>
+        </div>
+    `;
+
+	accordion.innerHTML = accordionContent;
+	fileSection.appendChild(accordion);
+
+	ShowLoading(false);
+}
