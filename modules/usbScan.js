cdccf75f7f4c9294d129bec208f164be29dd56d0
hajgreen <mohmadr407@gmail.com>
2024-09-01 14:35:32 -0700

    fixed

--- modules/usbScan.js
+++ modules/usbScan.js
@@ -1,10 +1,2 @@
-const usb = require('usb');
-
-// فهرست کردن تمام دستگاه‌های USB متصل
-const devices = usb.getDeviceList();
-
-devices.forEach(device => {
-    console.log('USB Device:', device.deviceDescriptor);
-});
 
 module.exports = {};
